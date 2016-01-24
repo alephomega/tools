646e6a75216f97c2cc86d2d0d89b44594697478c
johan.ahn <johan@johanahnui-MacBook-Pro.local>
2016-01-25 03:30:49 +0900

    analytics script

---
+++ config.R
@@ -0,0 +1,74 @@
+
+config <- function(basetime) { 
+
+  list (
+    fs = "hdfs://on-hadoop-master1.daum.net:8020",
+    jt = "on-hadoop-master1.daum.net:8021",
+    jar = "analytics.jar",
+    
+    filter = list (
+      driver = "PostgreSQL",
+      user = "front4vp",
+      password = "onproject!1",
+      dbname = "valuepotion",
+      host = "pg-dashboard.cv0lwhza2lbz.ap-southeast-1.rds.amazonaws.com",
+      port = 5432L,
+      SQL = sprintf(
+        "SELECT a.client_id, a.timezone, TO_CHAR(((TO_TIMESTAMP('%s', 'YYYYMMDDHH24MI')::TIMESTAMP WITHOUT TIME ZONE AT TIME ZONE 'Asia/Seoul') AT TIME ZONE b.name), 'YYYYMMDD') AS basedate, TO_CHAR(b.utc_offset, 'HH24MI') AS offset FROM apps a, pg_timezone_names b WHERE a.company_id <> 5 AND (a.status_id = 2 or a.id in (SELECT DISTINCT app_id FROM tracking_links)) AND a.timezone = b.name AND TO_CHAR(((TO_TIMESTAMP('%s', 'YYYYMMDDHH24MI')::TIMESTAMP WITHOUT TIME ZONE AT TIME ZONE 'Asia/Seoul') AT TIME ZONE b.name), 'HH24MI') BETWEEN '0000' AND '0059'",
+        basetime, basetime
+      )
+    ),
+    
+    job = list (
+      base_dir = "/user/hdfs/analytics",
+      tasks = list (
+        daily_summary = list (
+          main = "com.valuepotion.analytics.DailySummaryAnalytics",
+          properties = list (
+            hive.metastore.uris = "thrift://on-hadoop-master2.daum.net:9083",
+            mapred.reduce.tasks = 30
+          ),
+          
+          input = list (
+            database = "valuepotion_real",
+            table = "rc_track_daily",
+            columns = c("p_clientid", "deviceid", "session", "revenueamount", "currency", "eventname", "eventid", "dt", "deviceos", "deviceosversion", "appversion", "devicemodelname", "country", "userinfo_birth", "userinfo_gender", "userinfo_level"),
+          ),
+          overwrite = TRUE
+        ),
+        
+        attribution = list (
+          main = "com.valuepotion.analytics.AttributionJoinAnalytics",
+          properties = list (
+            mapred.reduce.tasks = 10,
+            mapred.max.split.size = 67108864
+          ),
+          attribution_base_dir = "/user/hdfs/attribution/by_hour",
+          overwrite = TRUE
+        ),
+    
+        attributes = list (
+          main = "com.valuepotion.analytics.AttributesAnalytics",
+          properties = list (
+            mapred.reduce.tasks = 50,
+            mapred.job.shuffle.input.buffer.percent = 0.3,
+            mapred.max.split.size = 67108864
+          ),
+          
+          defection = 365L,
+          risk = 30L,
+          overwrite = TRUE
+        ),
+        
+        balancer = list (
+          main = "com.valuepotion.analytics.DoNothing",
+          properties = list (
+            mapred.reduce.tasks = 100,
+            mapred.max.split.size = 67108864
+          ),
+          overwrite = TRUE
+        )
+      )
+    )
+  )
+}
---
+++ main.R
@@ -0,0 +1,191 @@
+#!/usr/bin/Rscript --vanilla
+
+library(RPostgreSQL)
+library(MR1.CDH4.7.1)
+
+
+print.timestamp <- function() {
+  strftime(Sys.time(), format="%Y-%m-%d %H:%M:%S") 
+}
+
+is.empty <- function(x) {
+  identical(x, NA) || is.null(x) || length(x) == 0
+}
+
+basedir <- function() {
+  args <- commandArgs(trailingOnly = FALSE)
+  needle <- "--file="
+  m <- grep(needle, args)
+  if (length(m) > 0) {
+    f <- normalizePath(sub(needle, "", args[m]))
+  } else {
+    f <- normalizePath(sys.frames()[[1]]$ofile)
+  }
+  
+  dirname(f)
+}
+
+basetime <- function() {
+  args <- commandArgs(TRUE)
+  
+  t0 <- NA
+  m <- match("--basetime", args, 0L)
+  if (m) {
+    t0 <- as.integer(args[m + 1])
+  }
+  
+  t0
+}
+
+
+setwd(basedir())
+source(config.R)
+
+cat(print.timestamp(), "Reading configurations.\n")
+conf <- config(basetime)
+print(conf)
+
+cat(print.timestamp(), "Querying target clients.\n")
+conn <- dbConnect(dbDriver(conf$filter$driver),
+                  user = conf$filter$user,
+                  password = conf$filter$password,
+                  dbname = conf$filter$dbname,
+                  host = conf$filter$host,
+                  port = as.integer(conf$filter$port))
+
+d <- dbGetQuery(conn, conf$filter$SQL)
+invisible(dbDisconnect(conn))
+
+if (nrow(d) > 0) {
+  d$basedate <- format(as.Date(d$basedate, format = "%Y%m%d") - 1, format = "%Y%m%d")
+  
+  d <- aggregate(client_id ~ (basedate + offset), FUN = c, data = d)
+  for (i in 1:nrow(d)) {
+    basedate <- d$basedate[i]
+    
+    if (grepl("^-", d$offset[i])) {
+      offset <- sprintf("B%04s", substr(d$offset[i], 2, length(d$offset[i])))  
+    } else {
+      offset <- sprintf("A%04s", d$offset[i])
+    }
+
+    d$client_id <- as.matrix(d$client_id)
+    filter <- sprintf("\"p_dt = '%s' and (%s)\"", basedate, paste(sprintf("p_clientid = '%s'", d$client_id[i, ]), collapse = " or "))
+    clients <- paste(d$client_id[i, ], collapse = ",")
+    
+    cat(print.timestamp(), sprintf("Running analytics job.\n", command))
+    task <- conf$job$tasks$daily_summary
+    args <- c(
+      "--base-date", basedate, 
+      "--database", task$input$database, 
+      "--table", task$input$table,
+      "--columns", task$input$columns,
+      "--filter", filter,
+      "--output", sprintf("%s/%s/%s/daily_summary", conf$job$base_dir, basedate, offset),
+      "--intermediate", sprintf("%s/%s/%s/daily_summary.tmp", conf$job$base_dir, basedate, offset)
+    )
+    
+    if (task$overwrite) {
+      args <- c(args, "--overwrite")
+    }
+    
+    cat(print.timestamp(), "Running daily-summary task.\n")
+    cat("properties\:n")
+    print(props)
+    cat("args:\n")
+    print(args)
+    
+    mr.run(
+      fs = conf$fs, 
+      jt = conf$jt, 
+      jar = file.path("lib", conf$jar), 
+      class = task$main,
+      args = args,
+      props = task$properties
+    )
+    
+    task <- conf$job$tasks$attribution
+    args <- c(
+      "--base-date", basedate, 
+      "--input", paste(sprintf("%s/%s/%s/daily_summary/*", conf$job$base_dir, basedate, offset), sprintf("%s/%s/**/part-*", task$attribution_base_dir, basedate), sep = ","),
+      "--output", sprintf("%s/%s/%s/daily_summary/attribution", conf$job$base_dir, basedate, offset),
+    )
+    
+    if (task$overwrite) {
+      args <- c(args, "--overwrite")
+    }
+    
+    cat(print.timestamp(), "Running attribution task.\n")
+    cat("properties\:n")
+    print(props)
+    cat("args:\n")
+    print(args)
+    
+    mr.run(
+      fs = conf$fs, 
+      jt = conf$jt, 
+      jar = file.path("lib", conf$jar), 
+      class = task$main,
+      args = args,
+      props = task$properties
+    )
+
+    task <- conf$job$tasks$attributes
+    args <- c(
+      "--base-date", basedate, 
+      "--defection", task$defection,
+      "--risk", task$risk,
+      "--input", paste(sprintf("%s/%s/%s/daily_summary/attribution/*", conf$job$base_dir, basedate, offset), sprintf("%s/customer_attributes/*", conf$job$base_dir), sep = ","),
+      "--output", sprintf("%s/%s/%s/customer_attributes", conf$job$base_dir, basedate, offset),
+    )
+    
+    if (task$overwrite) {
+      args <- c(args, "--overwrite")
+    }
+    
+    cat(print.timestamp(), "Running customer-attributes task.\n")
+    cat("properties\:n")
+    print(props)
+    cat("args:\n")
+    print(args)
+    
+    mr.run(
+      fs = conf$fs, 
+      jt = conf$jt, 
+      jar = file.path("lib", conf$jar), 
+      class = task$main,
+      args = args,
+      props = task$properties
+    )
+    
+    task <- conf$job$tasks$balancer
+    args <- c(
+      "--base-date", basedate, 
+      "--input", sprintf("%s/%s/%s/customer_attributes/\{NEW,ACTIVE,ATLISK,WINBACK,UNKNOWN\}-*", conf$job$base_dir, basedate, offset),
+      "--output", sprintf("%s/%s/%s/customer_attributes", conf$job$base_dir, basedate, offset),
+    )
+    
+    if (task$overwrite) {
+      args <- c(args, "--overwrite")
+    }
+    
+    cat(print.timestamp(), "Running balancer task.\n")
+    cat("properties\:n")
+    print(props)
+    cat("args:\n")
+    print(args)
+    
+    mr.run(
+      fs = conf$fs, 
+      jt = conf$jt, 
+      jar = file.path("lib", conf$jar), 
+      class = task$main,
+      args = args,
+      props = task$properties
+    )
+  }
+} else {
+  cat(print.timestamp(), "No clients to process.\n")
+}
+
+
